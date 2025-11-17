9049efee44fd44de646e3ad2e499cb5d01d06bba
Vladimir <masdor27@gmail.com>
2025-11-17 22:35:35 +0300

    Updates

--- README.md
+++
@@ -1,5 +0,0 @@
-# WEB-frontend
-
-## RIP_Frontend_Web_Service
-## Development of Internet applications
-Горшков В.М. ИУ5-53Б
---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/AstroDist-Frontend/sw.js', { scope: '/AstroDist-Frontend/' })})}
